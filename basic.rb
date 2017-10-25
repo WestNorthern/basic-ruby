d19f00769aa9d23b34debd71f1a682e93ec1b194
westnorthern <rrichardson610@gmail.com>
2017-10-25 15:23:13 -0600

    fixes #3

--- basic.rb
+++ basic.rb
@@ -1,9 +1,15 @@
+
+# Question One
+
 def only_in_america(string)
 	return string + " ... Only in America!"
 end
 
 puts only_in_america("Cat's that play fetch???")
 
+# Question Two
+
+# With array.each
 
 def max_value_in_array(array)
 	max = 0
@@ -16,9 +22,43 @@ def max_value_in_array(array)
 end
 
 
-puts max_value_in_array([11, 13, 5, 7])
+puts max_value_in_array [11, 13, 5, 7]
 
 new_test = [7, 9, 42, 0, -22]
 
-puts max_value_in_array(new_test)
+# With for loop
+
+def max_in_array(array)
+	max = 0
+	for i in 0...array.length
+		if array[i] > max
+			max = array[i]
+		end
+	end
+	return max
+end
+
+
+puts max_in_array new_test
+
+# Question Three
+
+country_array = [:Morocco, :Algeria, :Tunisia, :Libya, :Egypt]
+
+capital_array = ["Rabat", "Algiers", "Tunis", "Tripoli", "Cairo"]
+
+def combine_arrays(arr1,arr2)
+	newObj = {};
+	for i in 0...5 do 
+		newObj[arr1[i]] = arr2[i]
+
+
+	end
+
+	return newObj
+end
+
+puts combine_arrays country_array, capital_array
+
+
 
