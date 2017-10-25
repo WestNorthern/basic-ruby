ef83f7a4ac0e8b9e87509eb68752031044931b27
westnorthern <rrichardson610@gmail.com>
2017-10-25 12:28:14 -0600

    this commit fixes #1

--- basic.rb
+++ basic.rb
@@ -5,14 +5,14 @@ end
 puts only_in_america("Cat's that play fetch???")
 
 
-def max_value_in_array(array)
-	max_num
-	array.each { 
-		|x| if max_num < array[x]
-		max_num = array[x]
-	 }
-	return max_num
-end
+# def max_value_in_array(array)
+# 	max_num
+# 	array.each { 
+# 		|x| if max_num < array[x]
+# 		max_num = array[x]
+# 	 }
+# 	return max_num
+# end
 
 
-puts max_value_in_array([1, 3, 5, 7])
+# puts max_value_in_array([1, 3, 5, 7])
